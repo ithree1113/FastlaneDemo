b1e11a9f6074c7bb249b02b1e1701cd35a8673dc
ithree1113 <ithree1113@gmail.com>
2020-07-14 00:00:19 +0800

    Update the demo project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by EddieCheng on 2020/7/13.
+//  Copyright © 2020 EddieCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
